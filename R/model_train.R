cd64f04d2f9208db3ba72ff8f1c965b1b8304b80
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-01-26 08:30:53 +0530

    Update model_train.R

--- R/model_train.R
+++ R/model_train.R
@@ -1,5 +1,9 @@
 train_model <- function(df, target, features) {
   validate_basic(df, c(target, features))
-  formula <- stats::as.formula(paste(target, "~", paste(features, collapse = "+")))
+ if (length(features) == 0L) {
+    formula <- stats::as.formula(paste(target, "~ 1"))
+  } else {
+    formula <- stats::as.formula(paste(target, "~", paste(features, collapse = "+")))
+  }
   stats::lm(formula, data = df)
 }
