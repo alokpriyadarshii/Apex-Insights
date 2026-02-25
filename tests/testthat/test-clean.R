ddc6969ceb92c38a2ee4ba4de96187e1f3b45a4d
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-02-26 00:21:02 +0530

    Update test-clean.R

--- tests/testthat/test-clean.R
+++ tests/testthat/test-clean.R
@@ -4,3 +4,15 @@ testthat::test_that("clean_dataset cleans names", {
   testthat::expect_true("a_column" %in% names(out))
   testthat::expect_equal(out$a_column[1], "x")
 })
+
+testthat::test_that("clean_dataset validates input type", {
+  testthat::expect_error(clean_dataset(list(a = 1)), "must be a data.frame")
+})
+
+testthat::test_that("clean_dataset trims factor values", {
+  df <- data.frame(category = factor(c(" a ", "b  ")))
+  out <- clean_dataset(df)
+
+  testthat::expect_type(out$category, "character")
+  testthat::expect_equal(out$category, c("a", "b"))
+})
