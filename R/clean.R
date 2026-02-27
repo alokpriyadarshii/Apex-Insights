fa0c5e73e8dbc809d14a29ac320f92f0824664b6
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-02-27 19:30:57 +0530

    Update clean.R

--- R/clean.R
+++ R/clean.R
@@ -1,7 +1,7 @@
 clean_dataset <- function(df) {
-    if (!is.data.frame(df)) {
+  if (!is.data.frame(df)) {
     stop("`df` must be a data.frame")
-  
+  }
   df |>
     janitor::clean_names() |>
     dplyr::mutate(dplyr::across(dplyr::where(is.factor), as.character)) |>
