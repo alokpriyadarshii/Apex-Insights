4f4f1862db1d7e080342991c3f56409c2b4503fd
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-03-29 11:23:42 +0530

    Update io_write.R

--- R/io_write.R
+++ R/io_write.R
@@ -1,5 +1,5 @@
 write_artifact_rds <- function(obj, path) {
-  dir.create(dirname(path), recursive = TRUE, showWarnings = FALSE)
+  dir.create(dirname(path), recursive = TRUE, showWarnings = TRUE)
   readr::write_rds(obj, path)
   path
 }
